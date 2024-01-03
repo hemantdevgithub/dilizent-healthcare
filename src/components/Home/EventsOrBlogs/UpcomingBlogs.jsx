530aebd0c4bc030e576e8aec8086181dac46965f
Partho Roy <roypartho772@gmail.com>
2024-01-03 20:06:56 +0600

    hpome blog added

--- src/components/Home/EventsOrBlogs/UpcomingBlogs.jsx
+++ src/components/Home/EventsOrBlogs/UpcomingBlogs.jsx
@@ -1,24 +1,29 @@
+import { Link } from "react-router-dom";
+import { blogs } from "../../../constants/blogs";
+import BlogCard from "../../Blogs/BlogSection/BlogCard";
+import Container from "../../Container/Container";
 import BlogsOrEvents from "./BlogsOrEvents";
 
 export default function UpcomingBlogs() {
-  const data = [
-    {
-      id: 1,
-    },
-    {
-      id: 2,
-    },
-    {
-      id: 3,
-    },
-  ];
+
 
   return (
-    <div
-      className="
-    bg-[#F6F9FEAB]"
-    >
-      <BlogsOrEvents secName="Blog" data={data} />
-    </div>
+    <Container>
+      <div className="py-20 space-y-10">
+        <div className="flex justify-between">
+          <h1 className="text-2xl text-primary">Read Our All Blogs</h1>
+          <Link to={'/blogs'}>
+            <button className="bg-primary px-10 py-2 text-white font-poppins rounded-md">Blogs</button>
+          </Link>
+        </div>
+        <div
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"
+        >
+          {
+            blogs.slice(0, 3).map(b => <BlogCard key={b.id} {...b} />)
+          }
+        </div>
+      </div>
+</Container>
   );
 }
